88f7f87a31408bd52952ecda1a60bf7c6c4846c6
Adam Niedzielski <adamsunday@gmail.com>
2014-04-05 17:05:21 +0200

    Add additional fields to File

--- models/file.py
+++ models/file.py
@@ -3,4 +3,9 @@ from directory import *
 
 class File(BaseModel):
   name = CharField()
+  artist = CharField(null = True)
+  album = CharField(null = True)
+  title = CharField(null = True)
+  track_number = CharField(null = True)
+  duration = CharField(null = True)
   directory = ForeignKeyField(Directory, related_name = 'files')
